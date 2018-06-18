6528b47a97f772f7031b757ec6cc5681adc007f5
Jornad <jordan.watkins0922@gmail.com>
2018-06-17 22:57:24 -0400

    working rotor spinning

--- enigma.js
+++ enigma.js
@@ -10,6 +10,7 @@ updateRotor();
 
 function updateRotor() {
   var newline = "\r\n";
+
   var rot3Text = rot3Pos[25] + newline + rot3Pos[0] + newline + rot3Pos[1]
   rotor3.textContent = rot3Text;
 
@@ -18,4 +19,17 @@ function updateRotor() {
 
   var rot1Text = rot1Pos[25] + newline + rot1Pos[0] + newline + rot1Pos[1]
   rotor1.textContent = rot1Text;
+}
+
+function spinRotor(num) {
+  for(num >= 0; num--;) {
+    rot3Pos.unshift(rot3Pos.pop());
+    if(rot3[25] == "Z") {
+      rot2Pos.unshift(rot2Pos.pop());
+    }
+    if(rot2[25] == "Z") {
+      rot1Pos.unshift(rot1Pos.pop());
+    }
+  }
+  updateRotor();
 }
