0b7d10f04e3701cea1d5aa4505b0e0957a367f1d
Jornad <jordan.watkins0922@gmail.com>
2018-06-18 00:46:50 -0400

    added light board and data loading

--- enigma.js
+++ enigma.js
@@ -1,12 +1,42 @@
 var rot1 = document.getElementById('rotor1');
 var rot2 = document.getElementById('rotor2');
 var rot3 = document.getElementById('rotor3');
+var loadData = document.getElementById('loadSettings');
 
 var rot1Pos = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var rot2Pos = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var rot3Pos = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var plugs = {
+  A: "A",B: "B",C: "C",D: "D",E: "E",F: "F",G: "G",H: "H",I: "I",J: "J",K: "K",L: "L",M: "M",
+  N: "N",O: "O",P: "P",Q: "Q",R: "R",S: "S",T: "T",U: "U",V: "V",W: "W",X: "X",Y: "Y",Z: "Z"
+}
+
 updateRotor();
+
+function formatData() {
+  var data = loadData.value.split(",");
+  console.log(data);
+  updateData(parseInt(data[0]), parseInt(data[1]), parseInt(data[2]), data[3]);
+}
+
+function updateData(rot1Dat, rot2Dat, rot3Dat, plugs) {
+  for(rot1Dat >= 0; rot1Dat--;) {
+    rot1Pos.unshift(rot1Pos.pop());
+  }
+  for(rot2Dat >= 0; rot2Dat--;) {
+    rot2Pos.unshift(rot2Pos.pop());
+  }
+  for(rot3Dat >= 0; rot3Dat--;) {
+    rot3Pos.unshift(rot3Pos.pop());
+  }
+
+  var plugOrder = plugs.split("");
+  console.log(plugOrder);
+
+  updateRotor();
+  drawPlugs();
+}
 
 function updateRotor() {
   var newline = "\r\n";
@@ -32,4 +62,8 @@ function spinRotor(num) {
     }
   }
   updateRotor();
+}
+
+function drawPlugs() {
+
 }
