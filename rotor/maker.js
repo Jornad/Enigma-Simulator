744fa631bc971f00a67c377a5903a997097941b6
Jornad <jordan.watkins0922@gmail.com>
2018-06-28 21:38:42 -0400

    started a few background bits

---
+++ rotor/maker.js
@@ -0,0 +1,7 @@
+var rot1 = document.getElementById('data');
+var rot2 = document.getElementById('output');
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function formatRotor(dat) {
+  
+}
