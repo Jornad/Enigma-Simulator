d500ba70999bb56d34784eff33643c015341678f
Jornad <jordan.watkins0922@gmail.com>
2018-07-06 14:43:28 -0400

    worked on rotor formatting page some more

--- rotor/maker.js
+++ rotor/maker.js
@@ -1,7 +1,12 @@
-var rot1 = document.getElementById('data');
-var rot2 = document.getElementById('output');
+var data = document.getElementById('data');
+var rotNum = document.getElementById('rotnum');
+var output = document.getElementById('output');
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-function formatRotor(dat) {
-  
+function format() {
+  var numDat =[];
+  var dat = data.value.split("");
+  for (var i = 0; i < dat.length; i++) {
+    numDat[i] = alphabet.indexOf(dat[i].toUpperCase()) + 1;
+  }
 }
